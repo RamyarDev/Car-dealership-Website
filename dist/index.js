35409c07b393996f1990e1cd1de95022aa9ee028
Ramyar.IB <Ramyardev17@gmail.com>
2025-06-10 16:42:39 +0330

    Add some Typescript and finish the project

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,15 @@
 "use strict";
+const Categories = document.querySelectorAll(".Category");
+const Stars = document.querySelectorAll(".stars");
+Categories.forEach((e) => {
+    e.addEventListener("click", () => {
+        Categories.forEach((Category) => Category.classList.remove("active"));
+        e.classList.add("active");
+    });
+});
+Stars.forEach((star) => {
+    star.addEventListener("click", () => {
+        star.classList.toggle("fa-solid");
+        star.classList.toggle("text-yellow-500");
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+const Categories = document.querySelectorAll(".Category");
+const Stars = document.querySelectorAll(".stars");
+
+Categories.forEach((e) => {
+  e.addEventListener("click", () => {
+    Categories.forEach((Category) => Category.classList.remove("active"));
+    e.classList.add("active");
+  });
+});
+Stars.forEach((star) => {
+  star.addEventListener("click", () => {
+    star.classList.toggle("fa-solid");
+    star.classList.toggle("text-yellow-500");
+  });
+});
